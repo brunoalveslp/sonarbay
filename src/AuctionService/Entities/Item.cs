fcae4e0d11a88ae56056e302c4c6387393877cb8
Bruno Alves <brunoalveslp200597@gmail.com>
2025-07-31 22:53:52 -0300

    Basic Auction service created

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace AuctionService.Entities;
+
+[Table("Items")]
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int Mileage { get; set; }
+    public string ImageUrl { get; set; }
+    
+    // nav properties
+    [JsonIgnore]
+    public Auction Auction { get; set; }
+    public Guid AuctionId { get; set; }
+}
---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,36 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(cfg => { }, AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+app.Run();
